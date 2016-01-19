9e943a8e8702c7acc4267be002faec6995447fdb
ArtSyntax <arttra_eagleb@hotmail.com>
2016-01-19 22:23:33 +0700

    xlarge

--- app/src/main/java/com/example/artsyntax/fitnesstest/TestActivity.java
+++ app/src/main/java/com/example/artsyntax/fitnesstest/TestActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TestActivity extends ActionBarActivity {
+public class TestActivity extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
