b80aa4e080b21af445729a2bfad8631ad448f360
ArtSyntax <arttra_eagleb@hotmail.com>
2016-01-23 00:23:45 +0700

    MVC

---
+++ app/src/main/java/com/artsyntax/fitnesstest/MainApplication.java
@@ -0,0 +1,22 @@
+package com.artsyntax.fitnesstest;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+/**
+ * Created by ArtSyntax on 22/1/2559.
+ */
+public class MainApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor .getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/com/artsyntax/fitnesstest/activity/RecordingActivity.java
@@ -0,0 +1,15 @@
+package com.artsyntax.fitnesstest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.artsyntax.fitnesstest.R;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+    }
+}
